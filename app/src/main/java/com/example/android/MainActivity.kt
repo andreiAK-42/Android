2d0d8efb92c40f206877185ca04abd24a51e4fcb
andreiAK-42 <nyancat.project112@gmai.com>
2024-10-17 20:52:18 +0700

    add code

--- app/src/main/java/com/example/android/MainActivity.kt
+++ app/src/main/java/com/example/android/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.android
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val TAG = "Flickr cats"
+        val internetButton: Button = findViewById(R.id.btnHTTP)
+        internetButton.setOnClickListener{
+            var url = URL("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
+            Thread {
+                val connection1 = url.openConnection() as HttpURLConnection
+                try {
+                    val data = connection1.inputStream.bufferedReader().readText()
+                    connection1.disconnect()
+                    Log.d(TAG, data)
+                }
+                catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }.start()
         }
     }
 }
-//func
